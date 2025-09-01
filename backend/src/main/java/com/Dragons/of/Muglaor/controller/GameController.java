ae0e1d98678b538219548783a41d1ef707d5dd8f
Liisi Loomets <loomets@tlu.ee>
2025-09-01 15:40:26 +0300

    remove react crap

--- backend/src/main/java/com/Dragons/of/Muglaor/controller/GameController.java
+++ backend/src/main/java/com/Dragons/of/Muglaor/controller/GameController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000/")
 public class GameController {
 
     private GameService gameService;
