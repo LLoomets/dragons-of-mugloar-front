5faaf6aa5bdd9f3551babb7fd95e539443402f24
Liisi Loomets <loomets@tlu.ee>
2025-09-01 14:34:13 +0300

    initial commit

---
+++ backend/src/main/java/com/Dragons/of/Muglaor/DragonsOfMuglaorApplication.java
@@ -0,0 +1,13 @@
+package com.Dragons.of.Muglaor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonsOfMuglaorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonsOfMuglaorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Dragons/of/Muglaor/DragonsOfMuglaorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dragons.of.Muglaor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonsOfMuglaorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
